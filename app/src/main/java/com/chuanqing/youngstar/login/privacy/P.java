712f25721b2c406fc8de045ed107ff82783f740a
levy <2501908226@qq.com>
2019-01-02 17:17:08 +0800

    0102-开始MVP

---
+++ app/src/main/java/com/chuanqing/youngstar/login/privacy/P.java
@@ -0,0 +1,14 @@
+package com.chuanqing.youngstar.login.privacy;
+
+public class P {
+
+    private IView privacy;
+
+    public P(IView v){
+        privacy=v;
+    }
+
+    public void setContent(){
+//        privacy.setContent();
+    }
+}
--- app/src/main/java/com/chuanqing/youngstar/login/protocol/UserProtocolActivity.java
+++ app/src/main/java/com/chuanqing/youngstar/login/protocol/UserProtocolActivity.java
@@ -2,6 +2,7 @@ package com.chuanqing.youngstar.login.protocol;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.chuanqing.youngstar.R;
 import com.chuanqing.youngstar.base.BaseActivity;
@@ -11,5 +12,7 @@ public class UserProtocolActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protect);
+
+        TextView tv_protocol = findViewById(R.id.tv_protocol);
     }
 }
