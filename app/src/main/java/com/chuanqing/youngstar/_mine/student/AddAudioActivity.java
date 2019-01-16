72171acb2ed8207dfb46ef54d28be9b19e0afad1
levy <2501908226@qq.com>
2019-01-16 10:14:04 +0800

    0116

--- app/src/main/java/com/chuanqing/youngstar/_mine/student/AddAudioActivity.java
+++ app/src/main/java/com/chuanqing/youngstar/_mine/student/AddAudioActivity.java
@@ -1,6 +1,8 @@
 package com.chuanqing.youngstar._mine.student;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,7 +15,13 @@ public class AddAudioActivity extends BaseActivity implements View.OnClickListen
 
     private ImageView start;
     private ImageView pause;
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
 
+        }
+    };
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +41,16 @@ public class AddAudioActivity extends BaseActivity implements View.OnClickListen
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.iv_start_addaudio:
+                pause.setVisibility(View.VISIBLE);
+                start.setVisibility(View.INVISIBLE);
 
                 break;
             case R.id.iv_pause_addaudio:
+                pause.setVisibility(View.INVISIBLE);
+                start.setVisibility(View.VISIBLE);
 
                 break;
         }
     }
+
 }
