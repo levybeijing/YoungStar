a5da13907a65a34d79188d2e1684880a521acf09
li <2501908226@qq.com>
2019-03-20 10:52:57 +0800

    phase save

--- app/src/main/java/com/chuanqing/youngstar/_mine/EditPWD2Activity.java
+++ app/src/main/java/com/chuanqing/youngstar/_mine/EditPWD2Activity.java
@@ -6,11 +6,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chuanqing.youngstar.R;
 import com.chuanqing.youngstar.Urls;
 import com.chuanqing.youngstar.base.BaseActivity;
+import com.chuanqing.youngstar.login.bean.CommenBean;
 import com.chuanqing.youngstar.tools.SharedPFUtils;
+import com.google.gson.Gson;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
 
@@ -37,7 +40,6 @@ public class EditPWD2Activity extends BaseActivity implements View.OnClickListen
         et_old = findViewById(R.id.et_oldpwd_editpwd2);
         et_new = findViewById(R.id.et_newpwd_editpwd2);
         et_again = findViewById(R.id.et_pwdagain_editpwd2);
-
     }
 
     @Override
@@ -46,6 +48,19 @@ public class EditPWD2Activity extends BaseActivity implements View.OnClickListen
             case R.id.tv_ok_editpwd2:
                 String old = et_old.getText().toString().trim();
                 String mew = et_new.getText().toString().trim();
+                String again = et_again.getText().toString().trim();
+                if (old==null||old.length()==0){
+                    Toast.makeText(this, "原始密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (mew.length()<8){
+                    Toast.makeText(this, "密码长度不能小于8位", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!mew.equals(again)){
+                    Toast.makeText(this, "前后密码输入不相同", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 OkGo.post(Urls.updateUserPassword)//
                         .tag(this)//
                         .params("userCode", (String) SharedPFUtils.getParam(this,"usercode",""))//墙的ID
@@ -54,8 +69,13 @@ public class EditPWD2Activity extends BaseActivity implements View.OnClickListen
                         .execute(new StringCallback() {
                             @Override
                             public void onSuccess(String s, Call call, Response response) {
-                                Log.e("=============", "getSms"+s);
-                                finish();
+                                CommenBean bean = new Gson().fromJson(s, CommenBean.class);
+                                if (bean.getMessage().equals("请求成功")){
+                                    Toast.makeText(EditPWD2Activity.this, "修改成功", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }else{
+                                    Toast.makeText(EditPWD2Activity.this, ""+bean.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
                             }
                         });
                 break;
--- app/src/main/java/com/chuanqing/youngstar/_mine/EditPhone2Activity.java
+++ app/src/main/java/com/chuanqing/youngstar/_mine/EditPhone2Activity.java
@@ -1,6 +1,5 @@
 package com.chuanqing.youngstar._mine;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -13,48 +12,48 @@ import com.chuanqing.youngstar.R;
 import com.chuanqing.youngstar.Urls;
 import com.chuanqing.youngstar.base.BaseActivity;
 import com.chuanqing.youngstar.login.bean.CommenBean;
-import com.chuanqing.youngstar.mybean.FragCareCBean;
 import com.chuanqing.youngstar.tools.SharedPFUtils;
+import com.chuanqing.youngstar.tools.StringUtil;
 import com.google.gson.Gson;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
 
-import java.util.List;
-
 import okhttp3.Call;
 import okhttp3.Response;
 
-public class EditPhoneActivity extends BaseActivity implements View.OnClickListener {
+public class EditPhone2Activity extends BaseActivity implements View.OnClickListener {
 
     private EditText et_code;
-    private TextView tv_phone;
-    private String phone;
+    private EditText et_phone;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_changephone);
+        setContentView(R.layout.activity_changephone2);
 
-        phone = (String) SharedPFUtils.getParam(this, "phone", "");
         initView();
     }
 
     private void initView() {
-
-        tv_phone = findViewById(R.id.tv_phone_changephone);
-        et_code = findViewById(R.id.et_code_changephone);
-        tv_phone.setText(phone);
-
-        findViewById(R.id.tv_getcode_changephone).setOnClickListener(this);
-        TextView tochange = findViewById(R.id.tv_tochange_changephone);
+//
+        et_phone = findViewById(R.id.et_phone_changephone2);
+        et_code = findViewById(R.id.et_code_changephone2);
+//
+        findViewById(R.id.tv_getcode_changephone2).setOnClickListener(this);
+//
+        TextView tochange = findViewById(R.id.tv_tochange_changephone2);
         tochange.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.tv_getcode_changephone:
+                String phone = et_phone.getText().toString().trim();
+                if (!StringUtil.isPhoneNumber(phone)){
+                    Toast.makeText(this, "请输入正确手机号", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 OkGo.post(Urls.getSms)//
                         .tag(this)//
                         .params("mobile", phone)//墙的ID
@@ -66,11 +65,15 @@ public class EditPhoneActivity extends BaseActivity implements View.OnClickListe
                         });
                 break;
             case R.id.tv_tochange_changephone:
+                String pho = et_phone.getText().toString().trim();
                 String code = et_code.getText().toString().trim();
-
+                if (!StringUtil.isPhoneNumber(pho)){
+                    Toast.makeText(this, "手机号错误", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 OkGo.post(Urls.checkSms)//
                     .tag(this)//
-                    .params("mobile", phone)//墙的ID
+                    .params("mobile", pho)//墙的ID
                     .params("smsCode", code)//墙的ID
                     .execute(new StringCallback() {
                         @Override
@@ -78,14 +81,32 @@ public class EditPhoneActivity extends BaseActivity implements View.OnClickListe
                             Log.e("=============", "getSms"+s);
                             CommenBean bean = new Gson().fromJson(s, CommenBean.class);
                             if (bean.getMessage().equals("请求成功")){
-                                startActivity(new Intent(EditPhoneActivity.this,EditPhone2Activity.class));
-                                finish();
+                                change(pho);
                             }else{
-                                Toast.makeText(EditPhoneActivity.this, ""+bean.getMessage(), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(EditPhone2Activity.this, ""+bean.getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
                 break;
         }
+    }
+    private void change(String phone){
+        OkGo.post(Urls.updateUserMobile)//
+                .tag(this)//
+                .params("userCode", (String) SharedPFUtils.getParam(this,"usercode",""))//文件名
+                .params("mobile", phone)//墙的ID
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(String s, Call call, Response response) {
+                        Log.e("=============", "updateUserMobile"+s);
+                        CommenBean bean = new Gson().fromJson(s, CommenBean.class);
+                        if (bean.getMessage().equals("请求成功")){
+                            Toast.makeText(EditPhone2Activity.this, "修改成功", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }else{
+                            Toast.makeText(EditPhone2Activity.this, ""+bean.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
